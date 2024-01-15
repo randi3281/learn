ab74a185d5f3ce2e092700024d7a88977457bb8d
randi3281 <randiafif2312@gmail.com>
2024-01-15 14:28:34 +0700

    randomlah

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 Route::get('/about', function () {
     return view('about');
 })->name('about');
